45df125d34a6387f801a652a2f2d2654fcf21bf6
Jay <jay.patel@outdoorsy.co>
2023-12-02 15:13:24 -0800

    day 2 part 2

--- src/challenges/day2/index.js
+++ src/challenges/day2/index.js
@@ -36,17 +36,37 @@ const parseGame = row => {
     rounds,
   };
 };
+
+const getMinimumPowerOfGame = game => {
+  // minimum number of cubes per color
+  const counts = ALLOWED_COLORS.reduce((zeroes, color) => {
+    zeroes[color] = 0;
+    return zeroes;
+  }, {});
+  game.rounds.forEach(round => {
+    Object.keys(round).forEach(color => {
+      counts[color] = Math.max(counts[color], round[color]);
+    });
+  });
+  return ALLOWED_COLORS.reduce((product, color) => counts[color] * product, 1);
+};
+
 const main = async () => {
   const path = join(getDirname(import.meta.url), 'input.txt');
   const rows = readFileRows(path);
-  const total = rows.reduce((sum, row) => {
-    const game = parseGame(row);
+  const games = rows.map(parseGame);
+  // For part 1
+  const total = games.reduce((sum, game) => {
     const gameHasIllegalRound = game.rounds.some(round => {
       return Object.keys(round).some(color => round[color] > MAX_CUBE_COUNTS[color]);
     });
     return gameHasIllegalRound ? sum : sum + game.id;
   }, 0);
   console.log(total);
+  // For part 2
+  const powerPerGame = games.map(getMinimumPowerOfGame);
+  const powerSum = powerPerGame.reduce((sum, val) => val + sum, 0);
+  console.log(powerSum);
 };
 
 main();
