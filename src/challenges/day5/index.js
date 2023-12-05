158b40bc2f53dfa0863be64122293dc4ab33a1f4
Jay <jay.patel@outdoorsy.co>
2023-12-05 11:26:19 -0800

    set up for part 2

--- src/challenges/day5/index.js
+++ src/challenges/day5/index.js
@@ -3,17 +3,21 @@ import { getDirname, readFileRows, parseNumbers } from '../../util.js';
 
 const getSeedsAndMaps = rows => {
   const rangesMatrix = [];
-  const seeds = [];
+  const seedRanges = []; // part 2
+  const seeds = []; // part 1
   rows.forEach((row, i) => {
     if (!i) {
       seeds.push(...parseNumbers(row));
+      seeds.forEach((num, i) => {
+        !(i % 2) && seedRanges.push([num, num + seeds[i + 1] - 1]);
+      });
     } else if (row.indexOf('map') > -1) {
       rangesMatrix.push([]);
     } else {
       rangesMatrix[rangesMatrix.length - 1].push(parseNumbers(row));
     }
   });
-  return { seeds, rangesMatrix };
+  return { seeds, seedRanges, rangesMatrix };
 };
 
 const getLocation = (seed, rangesMatrix) => {
@@ -32,8 +36,10 @@ const main = async () => {
   const path = join(getDirname(import.meta.url), 'input.txt');
   const rows = readFileRows(path);
   const data = getSeedsAndMaps(rows);
+  // part 1
   const seedsToLocations = data.seeds.map(seed => getLocation(seed, data.rangesMatrix));
   console.log(Math.min(...seedsToLocations));
+  // part 2
 };
 
 main();
